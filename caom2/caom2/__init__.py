eaa5d8bf1a4bc68d74cabe52b11b689e9e7ab4c4
Adrian Damian <Adrian.Damian@nrc.ca>
2016-11-24 10:13:28 -0800

    Made astropy_helpers git submodules

--- caom2/caom2/__init__.py
+++ caom2/caom2/__init__.py
@@ -81,6 +81,5 @@ from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-#    from .caom2tools import *
-    pass
+#if not _ASTROPY_SETUP_:
+#    from .caom2 import *
