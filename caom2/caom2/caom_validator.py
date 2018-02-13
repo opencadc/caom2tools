6686a46f820f3c216c2902b462715534bfea2c07
Sharon <Sharon.Goliath@nrc-cnrc.gc.ca>
2018-02-12 16:49:19 -0800

    s2171 Add keywords validator as part of the caom_validator module.

---
+++ caom2/caom2/caom_validator.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+# ***********************************************************************
+# ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+# *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+#
+#  (c) 2018.                            (c) 2018.
+#  Government of Canada                 Gouvernement du Canada
+#  National Research Council            Conseil national de recherches
+#  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+#  All rights reserved                  Tous droits réservés
+#
+#  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+#  expressed, implied, or               énoncée, implicite ou légale,
+#  statutory, of any kind with          de quelque nature que ce
+#  respect to the software,             soit, concernant le logiciel,
+#  including without limitation         y compris sans restriction
+#  any warranty of merchantability      toute garantie de valeur
+#  or fitness for a particular          marchande ou de pertinence
+#  purpose. NRC shall not be            pour un usage particulier.
+#  liable in any event for any          Le CNRC ne pourra en aucun cas
+#  damages, whether direct or           être tenu responsable de tout
+#  indirect, special or general,        dommage, direct ou indirect,
+#  consequential or incidental,         particulier ou général,
+#  arising from the use of the          accessoire ou fortuit, résultant
+#  software.  Neither the name          de l'utilisation du logiciel. Ni
+#  of the National Research             le nom du Conseil National de
+#  Council of Canada nor the            Recherches du Canada ni les noms
+#  names of its contributors may        de ses  participants ne peuvent
+#  be used to endorse or promote        être utilisés pour approuver ou
+#  products derived from this           promouvoir les produits dérivés
+#  software without specific prior      de ce logiciel sans autorisation
+#  written permission.                  préalable et particulière
+#                                       par écrit.
+#
+#  This file is part of the             Ce fichier fait partie du projet
+#  OpenCADC project.                    OpenCADC.
+#
+#  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+#  you can redistribute it and/or       vous pouvez le redistribuer ou le
+#  modify it under the terms of         modifier suivant les termes de
+#  the GNU Affero General Public        la “GNU Affero General Public
+#  License as published by the          License” telle que publiée
+#  Free Software Foundation,            par la Free Software Foundation
+#  either version 3 of the              : soit la version 3 de cette
+#  License, or (at your option)         licence, soit (à votre gré)
+#  any later version.                   toute version ultérieure.
+#
+#  OpenCADC is distributed in the       OpenCADC est distribué
+#  hope that it will be useful,         dans l’espoir qu’il vous
+#  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+#  without even the implied             GARANTIE : sans même la garantie
+#  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+#  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+#  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+#  General Public License for           Générale Publique GNU Affero
+#  more details.                        pour plus de détails.
+#
+#  You should have received             Vous devriez avoir reçu une
+#  a copy of the GNU Affero             copie de la Licence Générale
+#  General Public License along         Publique GNU Affero avec
+#  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+#  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+#                                       <http://www.gnu.org/licenses/>.
+#
+#  $Revision: 4 $
+#
+# ***********************************************************************
+#
+
+"""Perform validation of the content of an Observation."""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+
+def validate(observation):
+    """
+    Perform validation of the content of an Observation.
+
+    Throws AssertionError if the keywords that are members of various
+    observation and plane metadata do not meet structure criteria.
+
+    :param observation: The Observation (SimpleObservation or
+        CompositeObservation) to validate.
+    """
+    if observation is not None:
+        _validate_keywords(observation)
+
+
+def _validate_keywords(observation):
+    """
+    Perform validation for all the places where keywords are members of
+    Observations and Planes.
+
+    Throws AssertionError if the keywords that are members of various
+    observation and plane metadata do not meet structure criteria.
+
+    :param observation: The parent Observation (SimpleObservation or
+        CompositeObservation) to validate.
+    """
+    if observation.proposal is not None:
+        _validate_keyword('proposal.keywords', observation.proposal.keywords)
+    if observation.target is not None:
+        _validate_keyword('target.keywords', observation.target.keywords)
+    if observation.telescope is not None:
+        _validate_keyword('telescope.keywords', observation.telescope.keywords)
+    if observation.instrument is not None:
+        _validate_keyword('instrument.keywords',
+                           observation.instrument.keywords)
+    for plane_key in observation.planes.keys():
+        if observation.planes[plane_key].provenance is not None:
+            _validate_keyword('provenance.keywords',
+                              observation.planes[plane_key].provenance.keywords)
+
+
+def _validate_keyword(name, keywords):
+    """
+    Perform validation for a Set of keywords.
+
+    :param name: Logging parameter to identify where the keywords originate.
+    :param keywords: Set of keyword values to validate.
+    """
+    for keyword in keywords:
+        _assert_validate_keyword('', name, keyword)
+
+
+def _assert_validate_keyword(caller, name, keyword):
+    """
+    Keywords can contain any valid UTF-8 character except the pipe (|). The
+    pipe character is reserved for use as a separator in persistence
+    implementations so the list of keywords can be serialized in a single string
+    to support querying.
+
+    :param caller: Logging parameter - identifies which class called this
+        method.
+    :param name: Logging parameter - narrows the calling identification for
+        this method.
+    :param keyword: The parameter to check.
+    """
+    if keyword.find('|') != -1:
+        raise AssertionError(
+            '{}: invalid {}: may not contain pipe (|)'.format(caller, name))
---
+++ caom2/caom2/tests/test_caom_validator.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+# ***********************************************************************
+# ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+# *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+#
+#  (c) 2018.                            (c) 2018.
+#  Government of Canada                 Gouvernement du Canada
+#  National Research Council            Conseil national de recherches
+#  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+#  All rights reserved                  Tous droits réservés
+#
+#  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+#  expressed, implied, or               énoncée, implicite ou légale,
+#  statutory, of any kind with          de quelque nature que ce
+#  respect to the software,             soit, concernant le logiciel,
+#  including without limitation         y compris sans restriction
+#  any warranty of merchantability      toute garantie de valeur
+#  or fitness for a particular          marchande ou de pertinence
+#  purpose. NRC shall not be            pour un usage particulier.
+#  liable in any event for any          Le CNRC ne pourra en aucun cas
+#  damages, whether direct or           être tenu responsable de tout
+#  indirect, special or general,        dommage, direct ou indirect,
+#  consequential or incidental,         particulier ou général,
+#  arising from the use of the          accessoire ou fortuit, résultant
+#  software.  Neither the name          de l'utilisation du logiciel. Ni
+#  of the National Research             le nom du Conseil National de
+#  Council of Canada nor the            Recherches du Canada ni les noms
+#  names of its contributors may        de ses  participants ne peuvent
+#  be used to endorse or promote        être utilisés pour approuver ou
+#  products derived from this           promouvoir les produits dérivés
+#  software without specific prior      de ce logiciel sans autorisation
+#  written permission.                  préalable et particulière
+#                                       par écrit.
+#
+#  This file is part of the             Ce fichier fait partie du projet
+#  OpenCADC project.                    OpenCADC.
+#
+#  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+#  you can redistribute it and/or       vous pouvez le redistribuer ou le
+#  modify it under the terms of         modifier suivant les termes de
+#  the GNU Affero General Public        la “GNU Affero General Public
+#  License as published by the          License” telle que publiée
+#  Free Software Foundation,            par la Free Software Foundation
+#  either version 3 of the              : soit la version 3 de cette
+#  License, or (at your option)         licence, soit (à votre gré)
+#  any later version.                   toute version ultérieure.
+#
+#  OpenCADC is distributed in the       OpenCADC est distribué
+#  hope that it will be useful,         dans l’espoir qu’il vous
+#  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+#  without even the implied             GARANTIE : sans même la garantie
+#  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+#  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+#  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+#  General Public License for           Générale Publique GNU Affero
+#  more details.                        pour plus de détails.
+#
+#  You should have received             Vous devriez avoir reçu une
+#  a copy of the GNU Affero             copie de la Licence Générale
+#  General Public License along         Publique GNU Affero avec
+#  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+#  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+#                                       <http://www.gnu.org/licenses/>.
+#
+#  $Revision: 4 $
+#
+# ***********************************************************************
+#
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .. import caom_validator
+from .. import observation
+from .. import plane
+
+
+def test_assert_validate_keyword():
+    caom_validator._assert_validate_keyword(__name__, 'test', 'foo')
+    caom_validator._assert_validate_keyword(__name__, 'test', 'foo=42')
+    caom_validator._assert_validate_keyword(__name__, 'test', 'foo:42')
+    caom_validator._assert_validate_keyword(__name__, 'test', "tick'marks")
+    caom_validator._assert_validate_keyword(__name__, 'test',
+                                            'has multiple spaces')
+    exception_raised = False
+    try:
+        caom_validator._assert_validate_keyword(__name__, 'test', 'pipe|denied')
+    except AssertionError:
+        # successful test case
+        exception_raised = True
+    assert exception_raised
+
+
+def test_validate():
+    obs = observation.SimpleObservation('test_collection', 'test_obs_id',
+                                        observation.Algorithm('test_name'))
+    caom_validator.validate(obs)
+    obs = observation.CompositeObservation('test_collection', 'test_obs_id',
+                                           observation.Algorithm('test_name'),
+                                           proposal=observation.Proposal(
+                                               'test_proposal'),
+                                           telescope=observation.Telescope(
+                                               'test_telescope'),
+                                           instrument=observation.Instrument(
+                                               'test_instrument'),
+                                           target=observation.Target(
+                                               'test_targets'))
+    obs.algorithm.keywords = 'foo'
+    obs.proposal.keywords = set('foo=42')
+    obs.telescope.keywords = set('foo:42')
+    obs.instrument.keywords.add("tick'marks")
+    obs.target.keywords = set('has multiple spaces')
+    test_plane = plane.Plane('test_plane')
+    test_plane.provenance = plane.Provenance('test_provenance')
+    test_plane.provenance.keywords.add('pipe|denied')
+    obs.planes['test_plane'] = test_plane
+    exception_raised = False
+    try:
+        caom_validator.validate(obs)
+    except AssertionError as e:
+        # success test case
+        assert str(e).find('provenance.keywords') != -1
+        exception_raised = True
+    assert exception_raised
