f0d767b528571fab5ac1acb64d6afaefc8f82fc1
Helena Jeeves <hridolfo@gmail.com>
2018-02-16 16:59:46 -0800

    s2171, ta 8858: temporalWCS validator + basic tests. spatialWCS validator + basic tests. spectralWCS validator + basic tests (incomplete). needed: polarizationWCS validator & tests. Basic framework in place for tests and validators.

---
+++ caom2utils/caom2utils/tests/test_wcsvalidator.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+# ***********************************************************************
+# ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+# *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+#
+#  (c) 2016.                            (c) 2016.
+#  Government of Canada                 Gouvernement du Canada
+#  National Research Council            Conseil national de recherches
+#  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+#  All rights reserved                  Tous droits réservés
+#
+#  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+#  expressed, implied, or               énoncée, implicite ou légale,
+#  statutory, of any kind with          de quelque nature que ce
+#  respect to the software,             soit, concernant le logiciel,
+#  including without limitation         y compris sans restriction
+#  any warranty of merchantability      toute garantie de valeur
+#  or fitness for a particular          marchande ou de pertinence
+#  purpose. NRC shall not be            pour un usage particulier.
+#  liable in any event for any          Le CNRC ne pourra en aucun cas
+#  damages, whether direct or           être tenu responsable de tout
+#  indirect, special or general,        dommage, direct ou indirect,
+#  consequential or incidental,         particulier ou général,
+#  arising from the use of the          accessoire ou fortuit, résultant
+#  software.  Neither the name          de l'utilisation du logiciel. Ni
+#  of the National Research             le nom du Conseil National de
+#  Council of Canada nor the            Recherches du Canada ni les noms
+#  names of its contributors may        de ses  participants ne peuvent
+#  be used to endorse or promote        être utilisés pour approuver ou
+#  products derived from this           promouvoir les produits dérivés
+#  software without specific prior      de ce logiciel sans autorisation
+#  written permission.                  préalable et particulière
+#                                       par écrit.
+#
+#  This file is part of the             Ce fichier fait partie du projet
+#  OpenCADC project.                    OpenCADC.
+#
+#  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+#  you can redistribute it and/or       vous pouvez le redistribuer ou le
+#  modify it under the terms of         modifier suivant les termes de
+#  the GNU Affero General Public        la “GNU Affero General Public
+#  License as published by the          License” telle que publiée
+#  Free Software Foundation,            par la Free Software Foundation
+#  either version 3 of the              : soit la version 3 de cette
+#  License, or (at your option)         licence, soit (à votre gré)
+#  any later version.                   toute version ultérieure.
+#
+#  OpenCADC is distributed in the       OpenCADC est distribué
+#  hope that it will be useful,         dans l’espoir qu’il vous
+#  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+#  without even the implied             GARANTIE : sans même la garantie
+#  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+#  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+#  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+#  General Public License for           Générale Publique GNU AfferoF
+#  more details.                        pour plus de détails.
+#
+#  You should have received             Vous devriez avoir reçu une
+#  a copy of the GNU Affero             copie de la Licence Générale
+#  General Public License along         Publique GNU Affero avec
+#  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+#  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+#                                       <http://www.gnu.org/licenses/>.
+#
+#  $Revision: 4 $
+#
+# ***********************************************************************
+#
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# from astropy.wcs import Wcsprm
+from caom2utils import WcsValidator, validate_temporal_wcs, validate_spatial_wcs, validate_spectral_wcs
+from caom2 import Artifact, wcs, chunk, plane
+
+import pytest
+import logging
+
+
+def test_temporalwcs_validator():
+    timetest = TimeTestUtil()
+    good_temporal_wcs = timetest.good_wcs()
+    assert good_temporal_wcs.axis.function is not None
+
+    is_valid = validate_temporal_wcs(good_temporal_wcs)
+
+    assert is_valid
+
+
+def test_spatialwcs_validator():
+    spatialtest = SpatialTestUtil()
+    good_spatial_wcs = spatialtest.good_wcs()
+    assert good_spatial_wcs.axis.function is not None
+
+    is_valid = validate_spatial_wcs(good_spatial_wcs)
+
+    assert is_valid
+
+def test_spectralwcs_validator():
+    spectraltest = SpectralTestUtil()
+    good_spectral_wcs = spectraltest.good_wcs()
+    assert good_spectral_wcs.axis.function is not None
+
+    is_valid = validate_spectral_wcs(good_spectral_wcs)
+
+    assert is_valid
+
+def test_valid_wcs():
+    pass
+    # public void testValidWCS() {
+    #     Artifact a = null;
+    #     try {
+    #         a = dataGenerator.getTestArtifact(ProductType.SCIENCE);
+    #         Chunk c = a.getParts().iterator().next().getChunks().iterator().next();
+    #
+    #         // Populate all WCS with good values
+    #         c.position = dataGenerator.mkGoodSpatialWCS();
+    #         c.energy = dataGenerator.mkGoodSpectralWCS();
+    #         c.time = dataGenerator.mkGoodTemporalWCS();
+    #         c.polarization = dataGenerator.mkGoodPolarizationWCS();
+    #
+    #         CaomWCSValidator.validate(a);
+    #     } catch (Exception unexpected) {
+    #         log.error(UNEXPECTED_EXCEPTION + " validating artifact: " + a.toString(), unexpected);
+    #         Assert.fail(UNEXPECTED_EXCEPTION + a.toString() + unexpected);
+    #     }
+    #
+    # }
+
+
+class TimeTestUtil:
+
+    def __init__(self):
+        pass
+
+    def good_wcs(self):
+        px = float(0.5)
+        sx = float(54321.0)
+        nx = 200
+        ds = float(0.01)
+
+        return self.getTestFunction(self, px, sx*nx*ds, nx, ds)
+
+    def getTestFunction(self, complete, px, sx, nx, ds):
+        axis_1d = wcs.CoordAxis1D(wcs.Axis("UTC", "d"))
+
+        if complete:
+            wcs.exposure = 300.0
+            wcs.resolution = 0.1
+
+        temporal_wcs = chunk.TemporalWCS(axis_1d)
+        ref_coord = wcs.RefCoord(px, sx)
+        temporal_wcs.axis.function = wcs.CoordFunction1D(nx, ds, ref_coord)
+        return temporal_wcs
+
+
+class SpatialTestUtil:
+
+    def __init__(self):
+        pass
+
+    def good_wcs(self):
+        px = float(0.5)
+        py = float(0.5)
+        sx = float(20.0)
+        sy = float(10.0)
+        # not used in java code although declared?
+        # double dp = 1000.0;
+        # double ds = 1.0;
+        return self.getTestFunction(px, py, sx, sy, False)
+
+    def getTestFunction(self, px, py, sx, sy, gal):
+        axis1 = wcs.Axis("RA", "deg")
+        axis2 = wcs.Axis("DEC", "deg")
+
+        if gal:
+            axis1 = wcs.Axis("GLON", "deg")
+            axis2 = wcs.Axis("GLAT", "deg")
+
+        axis_2d = wcs.CoordAxis2D(axis1, axis2)
+
+        spatial_wcs = chunk.SpatialWCS(axis_2d)
+        spatial_wcs.coordsys = "ICRS"
+
+        wcs.coordsys = "ICRS";
+        if gal:
+            spatial_wcs.coordsys = None
+
+        wcs.equinox = None
+
+        # Simple frame set: 1000x1000 pixels, 1 pixel = 1.0e-3 deg
+        dim = wcs.Dimension2D(1000, 1000)
+        ref = wcs.Coord2D(wcs.RefCoord(px, sx), wcs.RefCoord(py, sy))
+        axis_2d.function = wcs.CoordFunction2D(dim, ref, 1.e-3, 0.0, 0.0, 1.0e-3);
+        return spatial_wcs;
+
+
+class SpectralTestUtil:
+
+    def __init__(self):
+        pass
+
+    def good_wcs(self):
+        px = float(0.5)
+        sx = float(400.0)
+        nx = float(200.0)
+        ds = float(1.0)
+        # SpectralWCS
+        energy_test_util = EnergyTestUtil();
+        energy = energy_test_util.getTestRange(True, px, sx * nx * ds, nx, ds)
+
+        c1 = wcs.RefCoord(0.5, 2000.0)
+        energy.axis.function = wcs.CoordFunction1D(100, 10.0, c1)
+        return energy
+
+
+class PolarizationTestUtil:
+
+    def __init__(self):
+        pass
+
+    def good_wcs(self):
+        pass
+
+
+class EnergyTestUtil:
+    def __init__(self):
+        self.BANDPASS_NAME = "H-Alpha-narrow"
+        self.TRANSITION = wcs.EnergyTransition("H", "alpha")
+
+    def getTestRange(self, complete, px, sx, nx, ds):
+        axis =  wcs.CoordAxis1D(wcs.Axis("WAVE", "nm"))
+        # log.debug("test axis: " + axis);
+        spectral_wcs = chunk.SpectralWCS(axis, "TOPOCENT")
+        if complete:
+            spectral_wcs.bandpassName = self.BANDPASS_NAME
+            spectral_wcs.restwav = 6563.0e-10 # meters
+            spectral_wcs.resolvingPower = 33000.0
+            spectral_wcs.transition = self.TRANSITION
+
+        c1 = wcs.RefCoord(px, sx)
+        c2 = wcs.RefCoord(px + nx, sx + nx * ds)
+        spectral_wcs.axis.range = wcs.CoordRange1D(c1, c2)
+        # log.debug("test range: " + axis.range);
+        return spectral_wcs
+
+
+def getTestArtifact(ptype):
+    pass
+        # throws URISyntaxException {
+        # Plane testPlane = getTestPlane(ptype);
+        # artifact = artifact.Artifact('uri:foo/bar', chunk.ProductType.SCIENCE, chunk.ReleaseType.DATA)
+        # return artifact
+
+    # create an artifact
+    # populate it with known good WCS values for position, energy, time, polarization,
+    # that's it.
+#     Artifact a = null;
+#     try {
+#         a = dataGenerator.getTestArtifact(ProductType.SCIENCE);
+#         Chunk c = a.getParts().iterator().next().getChunks().iterator().next();
+#
+#         // Populate all WCS with good values
+#         c.position = dataGenerator.mkGoodSpatialWCS();
+#         c.energy = dataGenerator.mkGoodSpectralWCS();
+#         c.time = dataGenerator.mkGoodTemporalWCS();
+#         c.polarization = dataGenerator.mkGoodPolarizationWCS();
+#
+#         CaomWCSValidator.validate(a);
+#     } catch (Exception unexpected) {
+#         log.error(UNEXPECTED_EXCEPTION + " validating artifact: " + a.toString(), unexpected);
+#         Assert.fail(UNEXPECTED_EXCEPTION + a.toString() + unexpected);
+#     }
+#
+# }
+
+
+# dimension = wcs.Dimension2D(int(1), int(2))
+# ref_coord = wcs.Coord2D(wcs.RefCoord(float(9.0), float(10.0)),
+#                         wcs.RefCoord(float(11.0), float(12.0)))
+# cd11 = float(1.1)
+# cd12 = float(1.2)
+# cd21 = float(2.1)
+# cd22 = float(2.2)
---
+++ caom2utils/caom2utils/wcsvalidator.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+# ***********************************************************************
+# ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+# *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+#
+#  (c) 2016.                            (c) 2016.
+#  Government of Canada                 Gouvernement du Canada
+#  National Research Council            Conseil national de recherches
+#  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+#  All rights reserved                  Tous droits réservés
+#
+#  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+#  expressed, implied, or               énoncée, implicite ou légale,
+#  statutory, of any kind with          de quelque nature que ce
+#  respect to the software,             soit, concernant le logiciel,
+#  including without limitation         y compris sans restriction
+#  any warranty of merchantability      toute garantie de valeur
+#  or fitness for a particular          marchande ou de pertinence
+#  purpose. NRC shall not be            pour un usage particulier.
+#  liable in any event for any          Le CNRC ne pourra en aucun cas
+#  damages, whether direct or           être tenu responsable de tout
+#  indirect, special or general,        dommage, direct ou indirect,
+#  consequential or incidental,         particulier ou général,
+#  arising from the use of the          accessoire ou fortuit, résultant
+#  software.  Neither the name          de l'utilisation du logiciel. Ni
+#  of the National Research             le nom du Conseil National de
+#  Council of Canada nor the            Recherches du Canada ni les noms
+#  names of its contributors may        de ses  participants ne peuvent
+#  be used to endorse or promote        être utilisés pour approuver ou
+#  products derived from this           promouvoir les produits dérivés
+#  software without specific prior      de ce logiciel sans autorisation
+#  written permission.                  préalable et particulière
+#                                       par écrit.
+#
+#  This file is part of the             Ce fichier fait partie du projet
+#  OpenCADC project.                    OpenCADC.
+#
+#  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+#  you can redistribute it and/or       vous pouvez le redistribuer ou le
+#  modify it under the terms of         modifier suivant les termes de
+#  the GNU Affero General Public        la “GNU Affero General Public
+#  License as published by the          License” telle que publiée
+#  Free Software Foundation,            par la Free Software Foundation
+#  either version 3 of the              : soit la version 3 de cette
+#  License, or (at your option)         licence, soit (à votre gré)
+#  any later version.                   toute version ultérieure.
+#
+#  OpenCADC is distributed in the       OpenCADC est distribué
+#  hope that it will be useful,         dans l’espoir qu’il vous
+#  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+#  without even the implied             GARANTIE : sans même la garantie
+#  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+#  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+#  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+#  General Public License for           Générale Publique GNU Affero
+#  more details.                        pour plus de détails.
+#
+#  You should have received             Vous devriez avoir reçu une
+#  a copy of the GNU Affero             copie de la Licence Générale
+#  General Public License along         Publique GNU Affero avec
+#  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+#  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+#                                       <http://www.gnu.org/licenses/>.
+#
+#  $Revision: 4 $
+#
+# ***********************************************************************
+#
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+# from astropy.wcs import WCS
+# from cadcutils import version
+from caom2 import Artifact, Part, Chunk, Plane, Observation, CoordError
+# from caom2 import SpectralWCS,CoordAxis1D, Axis, CoordFunction1D, RefCoord
+# from caom2 import shape
+from astropy.wcs import Wcsprm
+from caom2utils import TimeUtil, EnergyUtil
+import numpy as np
+
+
+# from caom2util import TimeUtil
+
+APP_NAME = 'wcsvalidator'
+
+__all__ = ['WcsValidator', 'validate_polarization_wcs', 'validate_chunk', 'validate_temporal_wcs', 'validate_spatial_wcs', 'validate_spectral_wcs']
+
+
+# WcsValidator class and functions
+class WcsValidator(Artifact):
+    """
+    WcsValidator: validates WCS coordinates in the artifact passed in
+
+    Example:
+        ...
+
+    """
+    def __init__(self):
+        pass
+
+    def validate(self, artifact):
+        # for i, uri in enumerate(args.fileURI):
+        if artifact is not None:
+            for p in artifact.parts:
+                if p is not None:
+                    for c in p.chunks:
+                        context = artifact.uri + "[" + p.name + "]:" + c.id + " "
+                        self.validate_chunk(context, c)
+
+    def __str__(self):
+        pass
+
+
+
+def validate_chunk(context, chunk):
+    """
+
+    :param context:
+    :param chunk:
+    :return:
+
+    need to know what point to use for s2p, p2s transformation
+    """
+    validate_spatial_wcs(chunk.position)
+    validate_spectral_wcs(chunk.energy)
+    validate_temporal_wcs(chunk.time)
+    validate_polarization_wcs(chunk.polarization)
+
+def validate_spatial_wcs(position):
+    # position is a SpatialWCS
+    if position is not None and position.axis is not None:
+        if position.axis.function is not None:
+            fn2D = position.axis.function
+            naxis1_half = fn2D.dimension.naxis1/2
+            naxis2_half = fn2D.dimension.naxis2/2
+        #     use this as centre point for p2s translation
+        #  now into wcsprm...
+
+        # // need 2D array of doubles to pass in to p2s
+        wcsprm = Wcsprm()
+        coord_array = np.array([[naxis1_half, naxis2_half]])
+        # [[] * n for x in xrange(n)]
+
+        sky_transform = wcsprm.p2s(coord_array, 0)
+
+        pix_transform = wcsprm.s2p(sky_transform['world'], 0)
+
+        transformed_coords = pix_transform['pixcrd']
+        # TODO: not sure this is the right thing to have returned?
+        return transformed_coords[0][0] == naxis1_half and transformed_coords[0][1] == naxis2_half
+
+        # Errors from Java code that need to be handled
+        # Transform.Result tr = transform.sky2pix(coords)
+        #     }
+        # } catch (NoSuchKeywordException ex) {
+        #     throw new IllegalArgumentException(SPATIAL_WCS_VALIDATION_ERROR + ex.getMessage() + " in " + context, ex);
+        # } catch (WCSLibRuntimeException ex) {
+        #     throw new IllegalArgumentException(SPATIAL_WCS_VALIDATION_ERROR + ex.getMessage() + " in " + context , ex);
+        # } catch (UnsupportedOperationException ex) {
+        #     // error thrown from toPolygon if WCS is too near a pole, or if the bounds
+        #     // value is not recognized
+        #     throw new IllegalArgumentException(SPATIAL_WCS_VALIDATION_ERROR + ex.getMessage() + " in " + context, ex);
+        # }
+
+
+def validate_spectral_wcs(energy):
+    if energy is not None:
+        energyAxis = energy.axis
+        si = None
+        energy_util = EnergyUtil()
+
+        if energyAxis.range is not None:
+            si = energy_util.range1d_to_interval(energy, energyAxis.range)
+
+        if energyAxis.bounds is not None:
+            for tile in energyAxis.bounds.samples:
+                si = energy_util.range1d_to_interval(energy, tile)
+
+        if energyAxis.function is not None:
+            si = energy_util.function1d_to_interval(energy)
+
+            wcsprm = Wcsprm()
+            coord_array = np.array([[si[0], si[1]]])
+
+            sky_transform = wcsprm.p2s(coord_array, 0)
+            print(sky_transform)
+            pix_transform = wcsprm.s2p(sky_transform['world'], 0)
+            print(pix_transform)
+
+            transformed_coords = pix_transform['pixcrd']
+            print(transformed_coords)
+            print(si)
+
+            # TODO: not sure this is the right thing to have returned?
+            return transformed_coords[0][0] == si[0] and transformed_coords[0][1] == si[1]
+
+            # Exceptions from the Java code: does this get handled somehow here or not necessary?
+            # } catch (NoSuchKeywordException ex) {
+            # throw new IllegalArgumentException(SPECTRAL_WCS_VALIDATION_ERROR + ex.getMessage() + " in " + context, ex);
+            # } catch (WCSLibRuntimeException ex) {
+            # throw new IllegalArgumentException(SPECTRAL_WCS_VALIDATION_ERROR + ex.getMessage() + " in " + context, ex);
+            # }
+
+
+def validate_temporal_wcs(time):
+    timeutil = TimeUtil()
+    time_axis = time.axis
+
+    if time_axis.range is not None:
+        subinterval = timeutil.range1d_to_interval(time, time_axis.range)
+
+    if time_axis.bounds is not None:
+        for cr in time_axis.bounds.samples:
+            subinterval = timeutil.range1d_to_interval(time, cr)
+
+    if time_axis.function is not None:
+            print("function")
+            subinterval = timeutil.function1d_to_interval(time, time_axis.function)
+
+    print("validate_temporal_wcs subinterval: ")
+    print(subinterval)
+    return subinterval
+
+def validate_polarization_wcs(polarization):
+    pass
+
+
+
+
+
+
