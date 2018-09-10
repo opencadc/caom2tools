363987f20c1d3917dbeebc7fa616eb5975be6420
Adrian Damian <Adrian.Damian@nrc.ca>
2018-09-10 15:09:51 -0700

    Fixed unit test

--- caom2/caom2/tests/test_shape.py
+++ caom2/caom2/tests/test_shape.py
@@ -82,9 +82,9 @@ class TestEnums(unittest.TestCase):
         # test for invalid key
         with self.assertRaises(KeyError):
             shape.SegmentType["foo"]
-        with self.assertRaises(KeyError):
+        with self.assertRaises(TypeError):
             shape.SegmentType[None]
-        with self.assertRaises(KeyError):
+        with self.assertRaises(TypeError):
             shape.SegmentType[999]
         # test for invalid value
         with self.assertRaises(ValueError):
