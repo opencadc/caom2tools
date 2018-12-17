d804606b50ea2dfc8c12dad19b3aa81f2cab2249
Sharon <Sharon.Goliath@nrc-cnrc.gc.ca>
2018-12-17 13:08:58 -0800

    s2378 - flake8.

--- caom2/caom2/diff.py
+++ caom2/caom2/diff.py
@@ -411,7 +411,7 @@ def _get_dict(entity):
     for i in dir(entity):
         try:
             attribute = getattr(entity, i)
-        except TypeError as e:
+        except TypeError:
             pass
         if (i.startswith('_') or
                 callable(attribute) or
