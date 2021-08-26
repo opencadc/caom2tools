06b2d4da5b9a389e518d1cba9cf1ceac24604ef3
Adrian Damian <Adrian.Damian@nrc.ca>
2021-08-26 09:18:33 -0700

    Mark POSTs in caom2repo as idempotent so that the requests can be automatically re-retried on certain failures

--- caom2repo/caom2repo/core.py
+++ caom2repo/caom2repo/core.py
@@ -133,7 +133,8 @@ class CAOM2RepoClient(object):
         self.agent = agent
 
         self._repo_client = net.BaseWsClient(resource_id, subject,
-                                             agent, retry=True, host=self.host)
+                                             agent, retry=True, host=self.host,
+                                             idempotent_post=True)
         try:
             self._repo_client.caps.get_access_url(
                 CURRENT_CAOM2REPO_OBS_CAPABILITY_ID)
